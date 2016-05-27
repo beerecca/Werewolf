851223b4d2ad479ce450e60fe501ae765933b948
Ben Naylor <nayben@gmail.com>
2016-05-27 18:31:37 +1200

    changed input element, and some other stuff

--- app/components/Setup.js
+++ app/components/Setup.js
@@ -16,15 +16,15 @@ export default class Setup extends Component {
 			<span>
 				<h1>Werewolf</h1>
 				<form className="one-half offset-by-three column" onSubmit={this.handleSubmit.bind(this)}>
-					<div>
+					<div className="w-input__container">
 						<label className="w-label" htmlFor="name">Game Name</label>
 						<input type="text" ref="name" className="w-input" />
 					</div>
-					<div>
+					<div className="w-input__container">
 						<label className="w-label" htmlFor="moderator">Moderator Name</label>
 						<input type="text" ref="moderator" className="w-input" />
 					</div>
-					<button type="submit" className="button-primary">Create Game</button>
+					<button type="submit" className="w-btn__page-action button-primary">Create Game</button>
 				</form>
 			</span>
 		);
