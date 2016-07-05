f6e166a723ac404ac3048bf390bc93111efa82e6
Ben Naylor <nayben@gmail.com>
2016-07-05 21:09:19 +1200

    splitting dus sagas

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import createSagaMiddleware from 'redux-saga';
 //App Setup
 import { rootReducer } from './app/reducers';
 import { domReady } from './app/util/dom';
-import rootSaga from './app/sagas';
+import rootSaga from './app/sagas/sagas';
 import './app/style/style.scss';
 //App Controllers
 import AppController from './app/controllers/AppController';
